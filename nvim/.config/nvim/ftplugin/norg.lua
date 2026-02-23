7c861868c6d44d200d4c8e250cdb2e7fc5dacda5
markoplk <markopylypyuk@gmail.com>
2026-02-23 18:17:50 +0100

    chore(nvim): added modules and keybinds to norg files

--- nvim/.config/nvim/ftplugin/norg.lua
+++ nvim/.config/nvim/ftplugin/norg.lua
@@ -3,4 +3,6 @@ vim.opt_local.concealcursor = "nc"
 vim.opt_local.colorcolumn = "0"
 vim.opt_local.textwidth = 100
 
+vim.keymap.set("i", "<C-CR>", "<Plug>(neorg.itero.next-iteration)", { buffer = true })
+
 vim.treesitter.start()
--- nvim/.config/nvim/lua/plugins/neorg.lua
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -5,11 +5,24 @@ return {
         lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
         version = "*", -- Pin Neorg to the latest stable release
         dependencies = { "nvim-lua/plenary.nvim" },
+        ft = "norg", -- Lazy load on filetype
         config = function()
             require("neorg").setup({
                 load = {
                     ["core.defaults"] = {},
+                    ["core.itero"] = {},
+                    ["core.summary"] = {
+                        config = {
+                            strategy = "by_path"
+                        }
+                    },
+                    ["core.looking-glass"] = {},
                     ["core.concealer"] = {},
+                    ["core.keybinds"] = {
+                        config = {
+                            default_keybinds = true,
+                        }
+                    },
                     ["core.dirman"] = {
                         config = {
                             workspaces = {
@@ -21,6 +34,11 @@ return {
                 },
             })
         end,
+        keys = {
+            { "<LocalLeader>i", "<cmd>Neorg index<CR>", "n", desc = "Go to neorg index workspace" },
+            { "<LocalLeader>r", "<cmd>Neorg return<CR>", "n", desc = "Return from norg workspace" },
+            { "<LocalLeader>q", "<cmd>Neorg toc qflist<CR>", "n", desc = "Norg table of contents quickfix list" },
+        }
 
     }
 }
