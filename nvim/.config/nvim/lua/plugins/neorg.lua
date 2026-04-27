86b5dcee39986d271d5710c89c7d5a18c1640006
markoplk <markopylypyuk@gmail.com>
2026-04-27 21:29:14 +0200

    chore(plugins/neorg): removed autocmd from tree-sitter neorg build

--- nvim/.config/nvim/lua/plugins/neorg.lua
+++ nvim/.config/nvim/lua/plugins/neorg.lua
@@ -1,10 +1,8 @@
 return {
     {
         "nvim-neorg/neorg",
-        build = ":Neorg sync-parsers",
         lazy = false, -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
         version = "*", -- Pin Neorg to the latest stable release
-        dependencies = { "nvim-lua/plenary.nvim" },
         ft = "norg", -- Lazy load on filetype
         config = function()
             require("neorg").setup({
@@ -26,7 +24,7 @@ return {
                     ["core.dirman"] = {
                         config = {
                             workspaces = {
-                                notes = "~/notas",
+                                notes = "~/neorg/notas",
                             },
                             default_workspace = "notes",
                         },
